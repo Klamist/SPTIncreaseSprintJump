a7d541f0924f44ace5f5ebc0fa0295d34c4d233d
Klamist <33776012+Klamist@users.noreply.github.com>
2026-03-02 05:14:07 +0800

    2026-3-2 v1.0.0

---
+++ SuperSprintJump.cs
@@ -0,0 +1,142 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using Comfort.Common;
+using EFT;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SuperSprintJump
+{
+    [BepInPlugin("ciallo.SuperSprintJump", "Super Sprint Jump", "1.0.0")]
+    public class SpeedControlPlugin : BaseUnityPlugin
+    {
+        private static ConfigEntry<float> sprintSpeedMultiplier;
+        private static ConfigEntry<float> jumpMultiplier;
+        private static ConfigEntry<bool> noFallDamage;
+        private Harmony harmony;
+        private static ManualLogSource logger;
+
+        void Awake()
+        {
+            logger = Logger;
+
+            sprintSpeedMultiplier = Config.Bind("General", "Sprint Speed", 1.0f,
+                new ConfigDescription("Player sprint speed multiplier",
+                new AcceptableValueRange<float>(1.0f, 2.0f)));
+
+            jumpMultiplier = Config.Bind("General", "Jump Height", 1.0f,
+                new ConfigDescription("Player jump height multiplier",
+                new AcceptableValueRange<float>(1.0f, 10.0f)));
+
+            noFallDamage = Config.Bind("General", "NoFallDamage", false,
+                new ConfigDescription("Fall damage is auto divided by Jump Height coef. This is for zero fall damage"));
+
+            harmony = new Harmony("ciallo.SuperSprintJump");
+            harmony.PatchAll();
+        }
+
+        void OnDestroy()
+        {
+            harmony?.UnpatchSelf();
+        }
+
+        // 跳跃高度补丁
+        [HarmonyPatch(typeof(JumpStateClass), "Enter")]
+        class Patch_JumpEnter
+        {
+            static void Postfix(JumpStateClass __instance)
+            {
+                var gameWorld = Singleton<GameWorld>.Instance;
+                if (gameWorld?.MainPlayer == null) return;
+
+                var playerField = AccessTools.Field(typeof(MovementContext), "_player");
+                Player player = (Player)playerField.GetValue(__instance.MovementContext);
+
+                if (player == gameWorld.MainPlayer)
+                {
+                    __instance.Vector3_1 *= Mathf.Sqrt(jumpMultiplier.Value);
+                }
+            }
+        }
+
+        // 摔落伤害补丁
+        [HarmonyPatch(typeof(EFT.HealthSystem.ActiveHealthController), "HandleFall")]
+        class Patch_HandleFall
+        {
+            static bool Prefix(EFT.HealthSystem.ActiveHealthController __instance, float height, ref float __result)
+            {
+                var gameWorld = Singleton<GameWorld>.Instance;
+                if (gameWorld?.MainPlayer == null) return true;
+
+                if (__instance.Player == gameWorld.MainPlayer)
+                {
+                    if (noFallDamage.Value)
+                    {
+                        __result = 0f;
+                        return false;
+                    }
+                    else if (jumpMultiplier.Value > 1.0f)
+                    {
+                        float num = height - __instance.FallSafeHeight_1;
+                        if (!num.Positive())
+                        {
+                            __result = 0f;
+                            return false;
+                        }
+
+                        float damage = num * Mathf.Sqrt(num) *
+                                       GClass3009<EFT.HealthSystem.ActiveHealthController.GClass3008>.GClass1728_0.Falling.DamagePerMeter *
+                                       __instance.Player.Physical.FallDamageMultiplier;
+
+                        damage /= jumpMultiplier.Value;
+
+                        __instance.ApplyDamage(EBodyPart.LeftLeg, damage, GClass3051.FallDamage);
+                        __instance.ApplyDamage(EBodyPart.RightLeg, damage, GClass3051.FallDamage);
+
+                        __result = damage;
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        // 本地玩家冲刺时速度倍率
+        [HarmonyPatch(typeof(MovementContext), "get_ClampedSpeed")]
+        class Patch_ClampedSpeed
+        {
+            static void Postfix(MovementContext __instance, ref float __result)
+            {
+                var gameWorld = Singleton<GameWorld>.Instance;
+                if (gameWorld?.MainPlayer == null) return;
+
+                var playerField = AccessTools.Field(typeof(MovementContext), "_player");
+                Player player = (Player)playerField.GetValue(__instance);
+
+                if (player == gameWorld.MainPlayer && __instance.IsSprintEnabled)
+                {
+                    __result *= SpeedControlPlugin.sprintSpeedMultiplier.Value;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(MovementState), "ApplyMotion")]
+        class Patch_ApplyMotion
+        {
+            static void Prefix(ref Vector3 motion, float deltaTime, MovementState __instance)
+            {
+                var gameWorld = Singleton<GameWorld>.Instance;
+                if (gameWorld?.MainPlayer == null) return;
+
+                var playerField = AccessTools.Field(typeof(MovementContext), "_player");
+                Player player = (Player)playerField.GetValue(__instance.MovementContext);
+
+                if (player == gameWorld.MainPlayer && __instance.MovementContext.IsSprintEnabled)
+                {
+                    motion *= SpeedControlPlugin.sprintSpeedMultiplier.Value;
+                }
+            }
+        }
+    }
+}
