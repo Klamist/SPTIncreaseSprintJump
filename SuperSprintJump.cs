b6532010737c9c57a19e750ffc3a58ee48a26a8f
Klamist <33776012+Klamist@users.noreply.github.com>
2026-03-02 17:27:43 +0800

    2026-3-2 v1.1 break sprint speed limit

--- SuperSprintJump.cs
+++ SuperSprintJump.cs
@@ -8,10 +8,11 @@ using UnityEngine;
 
 namespace SuperSprintJump
 {
-    [BepInPlugin("ciallo.SuperSprintJump", "Super Sprint Jump", "1.0.0")]
+    [BepInPlugin("ciallo.SuperSprintJump", "Super Sprint Jump", "1.1.0")]
     public class SpeedControlPlugin : BaseUnityPlugin
     {
         private static ConfigEntry<float> sprintSpeedMultiplier;
+        private static ConfigEntry<bool> speedBreakLimit;
         private static ConfigEntry<float> jumpMultiplier;
         private static ConfigEntry<bool> noFallDamage;
         private Harmony harmony;
@@ -22,15 +23,18 @@ namespace SuperSprintJump
             logger = Logger;
 
             sprintSpeedMultiplier = Config.Bind("General", "Sprint Speed Multiplier", 1.0f,
-                new ConfigDescription("Only affect yourself",
-                new AcceptableValueRange<float>(1.0f, 2.0f)));
-
-            jumpMultiplier = Config.Bind("General", "Jump Height Multiplier", 1.0f,
-                new ConfigDescription("If this > 2.0, Sprint Speed will = origin * JumpHeight / 2",
+                new ConfigDescription("Physical effect will limit actual speed to not higher than 1.5~2.0x",
                 new AcceptableValueRange<float>(1.0f, 20.0f)));
 
-            noFallDamage = Config.Bind("General", "NoFallDamage", false,
-                new ConfigDescription("Fall damage is auto divided by Jump Height coef. This will remove fall damage."));
+            speedBreakLimit = Config.Bind("General", "Sprint Speed Overlimit (?)", false,
+                new ConfigDescription("Break physical speed limit.\nBuggy: uphill obstacled and downhill floated"));
+
+            jumpMultiplier = Config.Bind("General", "Jump Height Multiplier", 1.0f,
+                new ConfigDescription("If lower than sprint coef, will limit jump distance.",
+                new AcceptableValueRange<float>(1.0f, 20.0f)));
+
+            noFallDamage = Config.Bind("General", "No Fall Damage (?)", false,
+                new ConfigDescription("Fall damage is auto divided by Jump Height coef. This is for remove fall damage."));
 
             harmony = new Harmony("ciallo.SuperSprintJump");
             harmony.PatchAll();
@@ -41,7 +45,7 @@ namespace SuperSprintJump
             harmony?.UnpatchSelf();
         }
 
-        // 跳跃高度补丁
+        // 跳跃高度
         [HarmonyPatch(typeof(JumpStateClass), "Enter")]
         class Patch_JumpEnter
         {
@@ -60,7 +64,7 @@ namespace SuperSprintJump
             }
         }
 
-        // 摔落伤害补丁
+        // 摔落伤害
         [HarmonyPatch(typeof(EFT.HealthSystem.ActiveHealthController), "HandleFall")]
         class Patch_HandleFall
         {
@@ -102,7 +106,7 @@ namespace SuperSprintJump
             }
         }
 
-        // 本地玩家冲刺时速度倍率
+        // 冲刺速度增加
         [HarmonyPatch(typeof(MovementContext), "get_ClampedSpeed")]
         class Patch_ClampedSpeed
         {
@@ -116,19 +120,7 @@ namespace SuperSprintJump
 
                 if (player == gameWorld.MainPlayer && __instance.IsSprintEnabled)
                 {
-                    float sprintMult = SpeedControlPlugin.sprintSpeedMultiplier.Value;
-                    float jumpMult = SpeedControlPlugin.jumpMultiplier.Value;
-
-                    float finalMult = sprintMult;
-
-                    if (jumpMult > sprintMult)
-                    {
-                        if (jumpMult > 2f)
-                        {
-                            finalMult *= (jumpMult / 2f);
-                        }
-                    }
-                    __result *= finalMult;
+                    __result *= SpeedControlPlugin.sprintSpeedMultiplier.Value;
                 }
             }
         }
@@ -146,22 +138,28 @@ namespace SuperSprintJump
 
                 if (player == gameWorld.MainPlayer && __instance.MovementContext.IsSprintEnabled)
                 {
-                    float sprintMult = SpeedControlPlugin.sprintSpeedMultiplier.Value;
-                    float jumpMult = SpeedControlPlugin.jumpMultiplier.Value;
+                    motion *= SpeedControlPlugin.sprintSpeedMultiplier.Value;
+                }
+            }
 
-                    float finalMult = sprintMult;
+            static void Postfix(MovementState __instance, Vector3 motion, float deltaTime)
+            {
+                if (!SpeedControlPlugin.speedBreakLimit.Value)
+                    return;
 
-                    if (jumpMult > sprintMult)
-                    {
-                        if (jumpMult > 2f)
-                        {
-                            finalMult *= (jumpMult / 2f);
-                        }
-                    }
-                    motion *= finalMult;
+                var gameWorld = Singleton<GameWorld>.Instance;
+                if (gameWorld?.MainPlayer == null) return;
+
+                var playerField = AccessTools.Field(typeof(MovementContext), "_player");
+                Player player = (Player)playerField.GetValue(__instance.MovementContext);
+
+                if (player == gameWorld.MainPlayer && __instance.MovementContext.IsSprintEnabled)
+                {
+                    Vector3 forward = player.Transform.forward;
+                    float mult = SpeedControlPlugin.sprintSpeedMultiplier.Value;
+                    player.Transform.position += forward * mult * deltaTime;
                 }
             }
         }
-
     }
 }
