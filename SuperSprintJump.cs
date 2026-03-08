4e656e1a7710b4e8a179c670fbac8c74a9b6ed64
Klamist <33776012+Klamist@users.noreply.github.com>
2026-03-08 10:01:16 +0800

    2026-3-8 新增快捷键与状态栏

--- SuperSprintJump.cs
+++ SuperSprintJump.cs
@@ -8,13 +8,24 @@ using UnityEngine;
 
 namespace SuperSprintJump
 {
-    [BepInPlugin("ciallo.SuperSprintJump", "Super Sprint Jump", "1.1.0")]
+    [BepInPlugin("ciallo.SuperSprintJump", "Super Sprint Jump", "1.2.0")]
     public class SpeedControlPlugin : BaseUnityPlugin
     {
         private static ConfigEntry<float> sprintSpeedMultiplier;
         private static ConfigEntry<bool> speedBreakLimit;
         private static ConfigEntry<float> jumpMultiplier;
         private static ConfigEntry<bool> noFallDamage;
+
+        private static ConfigEntry<float> sprintStep;
+        private static ConfigEntry<KeyboardShortcut> increaseSpeedKey;
+        private static ConfigEntry<KeyboardShortcut> decreaseSpeedKey;
+        private static ConfigEntry<float> jumpStep;
+        private static ConfigEntry<KeyboardShortcut> increaseJumpKey;
+        private static ConfigEntry<KeyboardShortcut> decreaseJumpKey;
+        private static ConfigEntry<bool> guiEnabled;
+        private static ConfigEntry<int> guiPosX;
+        private static ConfigEntry<int> guiPosY;
+
         private Harmony harmony;
         private static ManualLogSource logger;
 
@@ -38,8 +49,31 @@ namespace SuperSprintJump
             noFallDamage = Config.Bind("General", "No Fall Damage (?)", false,
                 new ConfigDescription("Fall damage is auto divided by Jump Height coef. This is for remove fall damage."));
 
+            sprintStep = Config.Bind("Hotkeys", "Sprint Changing Step", 0.25f);
+            decreaseSpeedKey = Config.Bind("Hotkeys", "Sprint Speed -", new KeyboardShortcut(KeyCode.None));
+            increaseSpeedKey = Config.Bind("Hotkeys", "Sprint Speed +", new KeyboardShortcut(KeyCode.None));
+            jumpStep = Config.Bind("Hotkeys", "Jump Changing Step", 0.5f);
+            decreaseJumpKey = Config.Bind("Hotkeys", "Jump Height -", new KeyboardShortcut(KeyCode.None));
+            increaseJumpKey = Config.Bind("Hotkeys", "Jump Height +", new KeyboardShortcut(KeyCode.None));
+
+            guiEnabled = Config.Bind("HUD", "Ingame HUD", true);
+            guiPosX = Config.Bind("HUD", "Text Position X", 100);
+            guiPosY = Config.Bind("HUD", "Text Position Y", Screen.height - 150);
+
             harmony = new Harmony("ciallo.SuperSprintJump");
             harmony.PatchAll();
+        }
+
+        void Update()
+        {
+            if (increaseSpeedKey.Value.MainKey != KeyCode.None && UnityInput.Current.GetKeyDown(increaseSpeedKey.Value.MainKey))
+                sprintSpeedMultiplier.Value = Mathf.Min(20f, sprintSpeedMultiplier.Value + sprintStep.Value);
+            if (increaseSpeedKey.Value.MainKey != KeyCode.None && UnityInput.Current.GetKeyDown(decreaseSpeedKey.Value.MainKey))
+                sprintSpeedMultiplier.Value = Mathf.Max(1.0f, sprintSpeedMultiplier.Value - sprintStep.Value);
+            if (increaseSpeedKey.Value.MainKey != KeyCode.None && UnityInput.Current.GetKeyDown(increaseJumpKey.Value.MainKey))
+                jumpMultiplier.Value = Mathf.Min(20f, jumpMultiplier.Value + jumpStep.Value);
+            if (increaseSpeedKey.Value.MainKey != KeyCode.None && UnityInput.Current.GetKeyDown(decreaseJumpKey.Value.MainKey))
+                jumpMultiplier.Value = Mathf.Max(1.0f, jumpMultiplier.Value - jumpStep.Value);
         }
 
         void OnDestroy()
@@ -161,6 +195,55 @@ namespace SuperSprintJump
                     Vector3 current_motion = new Vector3(motion.x, motion.y, motion.z) * (mult - 1f);
                     player.Transform.position += current_motion * deltaTime;
                 }
+            }
+        }
+
+        // GUI 显示
+        [HarmonyPatch(typeof(GameWorld), "OnGameStarted")]
+        class Patch_OnGameStarted
+        {
+            static void Postfix(GameWorld __instance)
+            {
+                __instance.GetOrAddComponent<SpeedJumpHUD>();
+            }
+        }
+        public class SpeedJumpHUD : MonoBehaviour
+        {
+            private Texture2D backgroundTex;
+            private void Awake()
+            {
+                backgroundTex = new Texture2D(1, 1);
+                backgroundTex.SetPixel(0, 0, new Color(0f, 0f, 0f, 0.3f)); // 半透明黑色
+                backgroundTex.Apply();
+            }
+
+            private void OnGUI()
+            {
+                if (!SpeedControlPlugin.guiEnabled.Value) return;
+                if (Singleton<GameWorld>.Instance?.MainPlayer == null) return;
+
+                GUIStyle style = new GUIStyle(GUI.skin.label);
+                style.fontSize = 16;
+                style.normal.textColor = Color.white;
+
+                string text = $"Sprint = {SpeedControlPlugin.sprintSpeedMultiplier.Value:F2}x\n" +
+                              $"Jump = {SpeedControlPlugin.jumpMultiplier.Value:F2}x";
+
+                Rect rect = new Rect(
+                    SpeedControlPlugin.guiPosX.Value,
+                    SpeedControlPlugin.guiPosY.Value,
+                    114, 42
+                );
+
+                GUI.DrawTexture(rect, backgroundTex);
+                GUI.Label(rect, text, style);
+
+            }
+
+            private void OnDestroy()
+            {
+                if (backgroundTex != null)
+                    Destroy(backgroundTex);
             }
         }
     }
